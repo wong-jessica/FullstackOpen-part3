7c86ff06bea20f3948d2f189c42cf472c73d6bb2
Jessica Wong <jessikw2@uci.edu>
2019-12-16 21:17:40 -0800

    heroku

---
+++ build/precache-manifest.c175f2b773097cc05ba32264225850ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc1bf19263324dcae7b2141ae754c9dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab1a94379849f19ce3a1",
+    "url": "/static/css/main.f557298b.chunk.css"
+  },
+  {
+    "revision": "23e2f6f01b64f0a505c5",
+    "url": "/static/js/2.9a9037c4.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.9a9037c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab1a94379849f19ce3a1",
+    "url": "/static/js/main.7f50f3d6.chunk.js"
+  },
+  {
+    "revision": "58628116c0c07dec1426",
+    "url": "/static/js/runtime-main.ec0486a7.js"
+  }
+]);
