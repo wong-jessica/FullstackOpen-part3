705e6c47b24a1d9930b829b21c7d76912f818fc2
Jessica Wong <jessikw2@uci.edu>
2019-12-22 21:32:32 -0800

    p3.d

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if(process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://fullstack:${password}@fullstackopen-o7lol.mongodb.net/note-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//     content: 'Mongoose DB !!',
+//     date: new Date(),
+//     important: false,
+// })
+
+// note.save().then(response => {
+//     console.log('note saved')
+//     mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
