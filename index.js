3fc6d9acfb6d6d2662b6f2cc16abd427a905bb91
Jessica Wong <jessikw2@uci.edu>
2019-12-30 19:59:45 -0800

    p3-4

--- index.js
+++ index.js
@@ -6,25 +6,9 @@ const cors = require('cors')
 // const mongoose = require('mongoose')
 const Note = require('./models/note.js')
 
-app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('build'))
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes.map(note => note.toJSON()))
-  })
-})
-
-app.get('/api/notes/:id', (req, res) => {
-  Note.findById(req.params.id).then(note => {
-    res.json(note.toJSON())
-  })
-})
+app.use(bodyParser.json())
 
 app.post('/api/notes', (req, res) => {
   const body = req.body
@@ -43,10 +27,63 @@ app.post('/api/notes', (req, res) => {
   })
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = Number(req.params.id)
-  notes = notes.filter(note => note.id !== id)
-  res.status(204).end()
+// const unknownEndpoint = (req, res) => {
+//   res.status(404).send({error: 'unknown endpoint'})
+// }
+// app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+  if(error.name === 'CastError' && error.kind === 'ObjectId') {
+    return res.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+app.use(errorHandler)
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+  Note.find({}).then(notes => {
+    res.json(notes.map(note => note.toJSON()))
+  })
+})
+
+app.get('/api/notes/:id', (req, res, next) => {
+  Note.findById(req.params.id)
+    .then(note => {
+      if(note) {
+        res.json(note.toJSON())
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const body = req.body
+
+  const updatedNote = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(req.params.id, updatedNote, {new: true})
+    .then(updatedNote => {
+      res.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (req, res, next) => {
+  Note.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
