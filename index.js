36ac7f09b1df3c580b306ba1ae45e15ca061b029
Jessica Wong <jessikw2@uci.edu>
2019-12-31 19:42:09 -0800

    'uibuild'

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 if(process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-require('dotenv').config()
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
