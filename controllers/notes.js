91a2ca5406c7f1effbc09d6917ba179dbc8ec43c
Jessica Wong <jessikw2@uci.edu>
2020-01-09 15:32:04 -0800

    p4.7

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,10 @@
 const route = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 route.get('/', async (req, res) => {
-    const notes = await Note.find({})
+    const notes = await Note
+        .find({}).populate('user', {username:1, name:1})
     res.json(notes.map(note => note.toJSON()))
 })
 
@@ -22,14 +24,19 @@ route.get('/:id', async (req, res, next) => {
 route.post('/', async (req, res, next) => {
     const body = req.body
 
+    const user = await User.findById(body.userId)
+
     const note = new Note({
         content: body.content,
         important: body.important || false,
         date: new Date(),
+        user: user._id
     })
 
     try {
         const savedNote = await note.save()
+        user.notes = user.notes.concat(savedNote._id)
+        await user.save()
         res.json(savedNote.toJSON())
     } catch(error) {
         next(error)
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,8 @@ const route = require('express').Router()
 const User = require('../models/user')
 
 route.get('/', async (req, res) => {
-    const users = await User.find({})
+    const users = await User
+        .find({}).populate('notes', {content:1, date:1})
     res.json(users.map(u => u.toJSON()))
 })
 
